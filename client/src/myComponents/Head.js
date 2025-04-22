bae0f302bad228ae2ce83228ab494144e9df5461
ualUsham <2022mmb1392@iitrpr.ac.in>
2025-04-23 00:58:56 +0530

    log out added

--- client/src/myComponents/Head.js
+++ client/src/myComponents/Head.js
@@ -1,8 +1,16 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { auth } from './firebase'; 
 
 const Head = () => {
+    const location = useLocation();
+    const navigate = useNavigate();
 
+    const handleLogout = async () => {
+        await auth.signOut();
+        sessionStorage.clear();
+        navigate("/");
+    };
 
     return (
         <nav className="navbar navbar-light bg-light shadow border-bottom rounded-5 py-5 position-sticky top-0 z-3">
@@ -12,6 +20,13 @@ const Head = () => {
                 
                 <div>
                     <Link to="/" className='text-secondary position-absolute top-50 start-0 translate-middle-y mt-4 ps-3 ' style={{ textDecoration: 'none' }} >Home</Link>
+
+                    {(location.pathname === "/member" || location.pathname === "/approver") && (
+                        <button onClick={handleLogout} className="btn btn-link text-secondary position-absolute top-50 start-0 translate-middle-y mt-4 ps-5" style={{ marginLeft: "60px", textDecoration: 'none' }}>
+                            <i class="fa-solid fa-power-off fs-5"></i> Logout
+                        </button>
+                    )}
+
                     <Link to="/about" className='text-secondary position-absolute top-50 end-0 translate-middle-y mt-4 pe-3 ' style={{ textDecoration: 'none' }} >About Us</Link>
                 </div>
 
