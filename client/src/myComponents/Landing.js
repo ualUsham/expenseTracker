635b0889fe82e52192d56bfd46cd04ad817c0ff3
ualUsham <2022mmb1392@iitrpr.ac.in>
2025-04-24 04:07:59 +0530

    new change

--- client/src/myComponents/Landing.js
+++ client/src/myComponents/Landing.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import './landing.css';
+import { auth } from './firebase';
 
 const Landing = () => {
     const [showModal, setShowModal] = useState(false);
